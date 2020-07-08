11a24a60f955b12b4ff40b41090d610b2f6e95fc
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-07-09 00:57:32 +0900

    testing

--- weather/web-build/register-service-worker.js
+++ weather/web-build/register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
-      .register('/weather_app_2020/expo-service-worker.js', { scope: '/weather_app_2020/' })
+      .register('/expo-service-worker.js', { scope: '/' })
       .then(function (info) {
         // console.info('Registered service-worker', info);
       })
